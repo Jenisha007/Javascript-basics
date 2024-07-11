a60cc229424155f00f6390ebb094d4edf765e331
Jenisha007 <97434706+Jenisha007@users.noreply.github.com>
2024-07-11 14:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+/*
+ARTHIMETIC OPERATOR
+*/
+var a= 4
+var b= 3
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+/*
+ASSIGNMNT OPERATOR
+*/
+let x=3
+let y=4
+console.log(x+=y);
+console.log(x*=y);
+console.log(x%=y);
+console.log(x**=y);
+/*
+COMPARISON OPERATOR
+*/
+let i=5
+let j=6
+console.log(i==j);
+console.log(i!=j);
+console.log(i>j);
+console.log(i<=j);
+
+/*
+STRING OPERATOR
+*/
+let h=5+5
+let k="JENI"+17
+console.log(h);
+console.log(k);
+/*
+LOGICAL OPERATOR
+*/
+let c=5
+let d=6
+console.log(c&&d);
+console.log(c||d);
+console.log(c!=d);
+
+/*
+BITWISE OPERATOR
+*/
+let l=5
+let m=6
+console.log(l&m);
+console.log(l|m);
+console.log(~m);
+console.log(l^m);
+console.log(l<<m);
+console.log(l>>m);
+console.log(l>>>m);
+
+/*TERNARY OPERATOR*/
+
+let result = (10>0) ? true : false;
+console.log(result)
+
+/*STRING METHODS*/
+
+let n="jenisha,mathew,jefery"
+Result =n.length
+console.log(Result);
+Result =n.charAt(0);
+console.log(Result);
+Result =n.slice(8,20);
+console.log(Result);
+
+/*ARRAY*/
+let arr = [11,12,13,14,15];
+console.log(arr);
+console.log(arr[4]);
+
+/*OBJECTS*/
+
+let obj =[
+	{name:"jenisha",age:20},
+	{name:"crossmay",age:60},
+	{name:"viju",age:50},
+]
+console.log(obj);
+console.table(obj);
